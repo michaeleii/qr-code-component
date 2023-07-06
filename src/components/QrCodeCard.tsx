09e719a2a63b42ac70519f84c34d1afe715202ee
Michael Lei <michaelleie0ca@gmail.com>
2023-07-06 15:39:08 -0700

    Made card more round

--- src/components/QrCodeCard.tsx
+++ src/components/QrCodeCard.tsx
@@ -6,8 +6,8 @@ interface QrCode {
 
 function QrCodeCard({ qrCode }: { qrCode: QrCode }) {
   return (
-    <div className="max-w-md text-center bg-white rounded-xl flex p-10 flex-col gap-6 drop-shadow-md">
-      <img src={qrCode.image} alt="QR code" className="rounded-xl" />
+    <div className="max-w-md text-center bg-white rounded-3xl flex p-10 flex-col gap-6 drop-shadow-md">
+      <img src={qrCode.image} alt="QR code" className="rounded-3xl" />
       <h1 className="text-3xl font-bold text-[hsl(218_44%_22%)]">
         {qrCode.title}
       </h1>
